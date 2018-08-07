57e0d4ee5fc0e2cc79b7c4dd1b77ef00ae604c62
Throwback74 <corey.slade@gmail.com>
2018-08-07 01:12:17 -0700

    update

--- Assets/Useful Code/callbacks-map.js
+++ Assets/Useful Code/callbacks-map.js
@@ -4,30 +4,30 @@ var map = function (inputArray, callback, print) {
     var newArr = [];
     if(print) {
         for (var i = 0; i < inputArray.length; i++) {
-           newArr.push(print(callback(inputArray[i])));
+            newArr.push(print(callback(inputArray[i])));
         }
     } 
     else {
         for (var i = 0; i < inputArray.length; i++) {
-           newArr.push(callback(inputArray[i]));
+            newArr.push(callback(inputArray[i]));
         }
         
     }
     return newArr;
-}
+};
 
 var timesBy = function (num) {
     return (function (num, index) {
         return num * index;
     }.bind(this, num));
-}
+};
 
 var print = function (cb) {
     return function (index) {
         console.log(index);
         return index;
     }.bind(this, cb)();
-}
+};
 
 var result = map(myArray, timesBy(3), print);
 console.log(result);
